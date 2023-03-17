40166a945fc986f86fe008646e6e3a29745ce5e0
aaronsleepy <aaron@kmong.com>
2023-03-17 22:48:34 +0900

    feat: scope builder & concurrency

--- src/main/kotlin/ong/km/aaron/basics/HelloWorld.kt
+++ src/main/kotlin/ong/km/aaron/basics/HelloWorld.kt
@@ -7,12 +7,17 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
     doWorld()
+    println("Done")
 }
 
 suspend fun doWorld() = coroutineScope {
     launch {
+        delay(2000L)
+        println("World 2")
+    }
+    launch {
         delay(1000L)
-        println("World!")
+        println("World 1")
     }
     println("Hello")
 }
